85b81769b4c5a511602bf7614ae85d5123a7ce01
Mishator <voyteh89@mail.ru>
2023-03-15 20:00:28 +0300

    Выполнена Задача 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -38,6 +39,45 @@ public class Main {
         System.out.println(date5);
         System.out.println();
 
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        for (int index = 0; index < array.length; index++) {
+            if (index == array.length - 1) {
+                System.out.println(array[index]);
+                break;
+            }
+            System.out.print(array[index] + ", ");
+        }
+        System.out.println();
+
+        double[] myArray = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(myArray));
+        System.out.println();
+
+        short[] myDate = new short[5];
+        myDate[0] = 1925;
+        short date1 = myDate[0];
+        myDate[1] = 1989;
+        short date2 = myDate[1];
+        myDate[2] = 1984;
+        short date3 = myDate[2];
+        myDate[3] = 2008;
+        short date4 = myDate[3];
+        myDate[4] = 2012;
+        short date5 = myDate[4];
+        for (int index = 0; index < myDate.length; index++) {
+            if (index == myDate.length - 1) {
+                System.out.println(myDate[index]);
+                break;
+            }
+            System.out.print(myDate[index] + ", ");
+        }
+        System.out.println();
     }
 
 }
